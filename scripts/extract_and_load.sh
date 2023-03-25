529e936b76722000b66c37ee3d0e8f745c6deef1
João Pedro Picolo <picolo@Joaos-MacBook-Pro.local>
2023-03-25 01:40:28 -0300

    Fixed small bug

--- scripts/extract_and_load.sh
+++ scripts/extract_and_load.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
+# Install the necessary packages
 apt-get update && apt-get install -y postgresql-client
+
+# Script to extract and load
+psql postgresql://postgres:password@analytics:5432/analytics -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public;"
 pg_dump postgresql://postgres:password@transactional:5432/dvdrental | psql postgresql://postgres:password@analytics:5432/analytics
